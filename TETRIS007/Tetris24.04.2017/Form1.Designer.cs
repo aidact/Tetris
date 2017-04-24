87fa18977ee09e85cae42a3e0fc632bf6684ca6a
aidact <aida.ct127@gmail.com>
2017-04-24 19:17:38 +0600

    SIS Project Tetris

---
+++ TETRIS007/Tetris24.04.2017/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Tetris24._04._2017
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.panel1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(257, 371);
+            this.panel1.TabIndex = 0;
+            this.panel1.TabStop = false;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Yellow;
+            this.button1.Location = new System.Drawing.Point(266, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 55);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(281, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(353, 361);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.panel1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TETRIS007/Tetris24.04.2017/Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Tetris24._04._2017
+{
+    public partial class Form1 : Form
+    {
+        const int MAXX = 10; // panel's width
+        const int MAXY = 15; // panel's height
+        const int ps = 24; 
+        bool started = false; // condition of starting
+        // colours for figures
+        Brush[] bru = {Brushes.White, Brushes.Orange, Brushes.Blue, Brushes.Red, Brushes.Green, Brushes.Honeydew,
+            Brushes.Violet, Brushes.Tomato, Brushes.SteelBlue, Brushes.PapayaWhip};
+
+        int score;
+        bool check = true;
+        int[,] ar; // matrix of board
+        Figure figure;
+        Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void upd_score()
+        {
+            label1.Text = "Score : " + score.ToString();
+        }
+
+        public class Figure
+        {
+            /*
+             * ....
+             * 1111
+             * ....
+             * ....
+             * 
+             * ....
+             * 111.
+             * 1...
+             * ....
+             * 
+             * ....
+             * .11.
+             * .11.
+             * ....
+             * 
+             * .1..
+             * .11.
+             * ..1.
+             * ....
+             * */
+            string[] nya = {"....1111........", // My cute figures
+                            "....111.1.......",
+                            ".....11..11.....", 
+                            ".1...11...1.....",
+                            "....111..1......", 
+                            "....111...1.....", 
+                            "..1..11..1......"};
+            public int x, y; 
+            public int clr;
+            public int[,] pix; // pixels of board
+            public Random rnd;
+
+            public Figure() // smthng like constructor : generates existing game from nya
+            {
+                x = 3; y = -2;
+                rnd = new Random();
+                clr = 1 + rnd.Next(9);
+                int n = rnd.Next(nya.Length);
+                fillpix(out pix, nya[n]);
+                n = rnd.Next(4);
+                for (int i = 0; i < n; ++i)
+                    rotate();
+            }
+
+            void fillpix(out int[,] pix, string cc)
+            {
+                pix = new int[4, 4];
+                int i, j;
+                for (i = 0; i < 4; ++i)
+                    for (j = 0; j < 4; ++j)
+                    {
+                        if (cc[j * 4 + i] == '1') pix[i, j] = clr; // если не пустой пиксел то заполнить цветом
+                        else pix[i, j] = 0; // пустая ячейка
+                    }
+            }
+            public void rotate() // condition of rotation
+            {
+                int[,] p2 = new int[4, 4]; // 4 to 4 matrix for each figure
+                int i, j;
+                for (i = 0; i < 4; ++i)
+                    for (j = 0; j < 4; ++j)
+                    {
+                        p2[3 - j, i] = pix[i, j];
+                    }
+                pix = p2;
+            }
+        }
+
+        private void draw(int x, int y, int clr)
+        {
+           
+           
+                if (clr == 0 || x < 0 || x >= MAXX || y < 0 || y >= MAXY) return;
+                g.FillEllipse(bru[clr], new Rectangle(2 + x * ps, 2 + y * ps, ps, ps));
+            
+        }
+
+        private void draw_field() // drawing the field
+        {
+            int i, j;
+            for (i = 0; i < MAXX; ++i)
+                for (j = 0; j < MAXY; ++j)
+                    draw(i, j, ar[i, j]);
+        }
+         
+        private void draw_figure(Figure f)
+        {
+            int i, j;
+            for (i = 0; i < 4; ++i)
+                for (j = 0; j < 4; ++j)
+                    draw(f.x + i, f.y + j, f.pix[i, j]);
+        }
+
+        private void button1_Click(object sender, EventArgs e) // start button
+        {
+            started = true;
+            button1.Enabled = false;
+            score = 0;
+            ar = new int[MAXX, MAXY];
+            upd_score(); 
+            figure = null; // no figures from the start
+            timer1.Enabled = true;
+        }
+
+        private int get_ar(int x, int y)
+        {
+            if (x < 0 || x >= MAXX || y >= MAXY) return 1; // если фигура переходит границы
+            if (y < 0) return 0; // если фигуры пока нет
+            return ar[x, y];
+        }
+
+        private void set_ar(int x, int y, int clr)
+        {
+            if (x < 0 || x >= MAXX || y < 0 || y >= MAXY) return;
+            ar[x, y] = clr;
+        }
+
+        private bool figure_ok() // Can we put the figure
+        {
+            int i, j;
+            for (i = 0; i < 4; ++i)
+                for (j = 0; j < 4; ++j)
+                {
+                    if (figure.pix[i, j] != 0 && get_ar(figure.x + i, figure.y + j) != 0)
+                        return false;
+                }
+            return true;
+        }
+
+        private void apply_figure() // Adds score if there any, sets current figure to the field
+        {
+            int i, j;
+            for (i = 0; i < 4; ++i)
+                for (j = 0; j < 4; ++j)
+                    if (figure.pix[i, j] != 0)
+                        set_ar(figure.x + i, figure.y + j, figure.pix[i, j]);
+            bool[] lines = new bool[MAXY]; // lines[i] = true if i-th line is filled fully
+            int numberOfFilledLines = 0; // number of filled lines
+            for (i = 0; i < MAXY; ++i)
+            {
+                bool isThisRowFilled = true;
+                for (j = 0; j < MAXX; ++j)
+                {
+                    if (ar[j, i] == 0)
+                    {
+                        isThisRowFilled = false;
+                        break;
+                    }
+                }
+                lines[i] = isThisRowFilled;
+                if (isThisRowFilled)
+                {
+                    ++numberOfFilledLines;
+                }
+            }
+            if (numberOfFilledLines != 0)
+            {
+                // Updating score
+                int sc = (numberOfFilledLines * (numberOfFilledLines + 1) / 2) * 100;
+                score ++;
+                upd_score();
+                //simpleSound.Play();
+            }
+            /*Updating our game field**/
+            int[,] nar = new int[MAXX, MAXY]; // nar is new ar
+            int ty = MAXY; // pointer to the row on top of new ar
+            for (i = MAXY - 1; i >= 0; --i)
+            {
+                if (false == lines[i])
+                {
+                    --ty;
+                    for (j = 0; j < MAXX; ++j)
+                    {
+                        nar[j, ty] = ar[j, i];
+                    }
+                }
+            }
+            ar = nar;
+        }
+
+        private void figure_down()
+        {
+            ++figure.y; // спукаться по у
+            if (!figure_ok())
+            {
+                --figure.y;
+                apply_figure();
+                figure = null;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (figure == null)
+            {
+                figure = new Figure();
+                if (!figure_ok())
+                {
+                    timer1.Enabled = false;
+                    /*if (score == 1)
+                    {
+                        MessageBox.Show("YOU WIN!");
+                    }*/
+                    MessageBox.Show("GAME OVER!");
+                    button1.Enabled = true;
+                }
+            }
+            //if(check == true) 
+                figure_down();
+            panel1.Refresh();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            if (!started) return;
+            g = e.Graphics;
+            g.DrawRectangle(Pens.LightBlue, new Rectangle(0, 0, ps * MAXX + 4, ps * MAXY + 4));
+            g.FillRectangle(Brushes.Black, new Rectangle(1, 1, ps * MAXX + 3, ps * MAXY + 3));
+            draw_field();
+            if (figure != null) draw_figure(figure);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (figure == null) return;
+            if (e.KeyCode == Keys.A)
+            {
+                figure.x -= 1;
+                if (!figure_ok()) figure.x += 1;
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                figure.x += 1;
+                if (!figure_ok()) figure.x -= 1;
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                figure.y += 1;
+                if (!figure_ok()) figure.y -= 1;
+            }
+            else if (e.KeyCode == Keys.W)
+            {
+                figure.rotate();
+                if (!figure_ok())
+                {
+                    for (int i = 0; i < 3; ++i) figure.rotate();
+                }
+            }
+            panel1.Refresh();
+        }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+        //  if(check == true) 
+              Form1_KeyDown(sender, e);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // panel1.enableD/`B;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       /* private void button2_Click(object sender, EventArgs e)
+        {
+            check = false;
+         // panel1.Image.Save("game");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            check = true;
+        }*/
+
+        
+    }
+}
